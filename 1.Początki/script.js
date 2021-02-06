6a1a951dcaf08337ff0cc354f8557ef842e905f5
damianduralek <72334876+damianduralek@users.noreply.github.com>
2021-02-06 20:23:18 +0100

    Update script.js

---
+++ 1.Początki/script.js
@@ -0,0 +1,21 @@
+//zadanie 1
+
+console.log("Twoje imię");
+
+//zadanie 2
+
+console.log(12 + 32);
+console.log(52 / 16);
+console.log(5 * 2);
+
+
+//zadanie 3
+
+    //wyświetli poprawnie
+console.log("Hello World");
+
+    //error, nie ma takiej zmiennej
+console.log(Hello World);
+
+    //syntax error
+console.log("Hello World);
