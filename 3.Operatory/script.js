4854f11f847ed5bb44e9f0969f6e73801b929bf3
damianduralek <72334876+damianduralek@users.noreply.github.com>
2021-02-07 22:49:03 +0100

    Update script.js

---
+++ 3.Operatory/script.js
@@ -0,0 +1,35 @@
+// Zadanie 1
+let a = true;
+let b = false;
+console.log(a == b);
+
+// Zadanie2
+let x = 5;
+let y = 10;
+let moduloResult = 0;
+x / y %= moduloResult;
+console.log(moduloResult);
+
+// Zadanie3
+let string1 = 'Lorem';
+let string2 = 'ipsum';
+let stringResult = string1 + ' ' + string2;
+console.log(stringResult);
+
+// Zadanie 4
+var someNumber = 425;
+var someString = '425';
+console.log(someNumber == someString); // porównuje wartości zmiennych
+console.log(someNumber === someString); // porównuje wartości i typ zmiennych
+
+// Zadanie 5
+let counter = 30;
+console.log(counter++);
+console.log(counter--);
+
+// Zadanie 6
+let num1 = 10;
+let num2 = 12;
+let result = null;
+result = num1 > num2;
+console.log(result);
