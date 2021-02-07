0311c524ee0ede7a4bdb14cdb34d4a7e761048ad
damianduralek <72334876+damianduralek@users.noreply.github.com>
2021-02-07 22:44:58 +0100

    Update script.js

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,26 @@
+// Zadanie 1
+
+const exampleNumber = 25;
+const exampleString = "lorem";
+const exampleNumberString = 3 + "lorem";
+const exampleBool = true;
+const exampleNull = null;
+console.log("exampleNumber: ", exampleNumber)
+console.log("exampleString: ", exampleString)
+console.log("exampleNumberString: ", exampleNumberString)
+console.log("exampleBool: ", exampleBool)
+console.log("exampleNull: ", exampleNull)
+
+// Zadanie 2
+
+let firstnumber = 20;
+let secondnumber = 30;
+let sum = 0;
+sum = firstnumber + secondnumber;
+console.log(sum);
+
+// Zadanie 3
+
+let exampleVariable;
+// return undefined - nothing has been assigned to the variable.
+console.log(exampleVariable);
